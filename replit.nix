e6197401e6d5140b97709e4901127552cc7d2a8f
mohamedeid-a <mohaamed.eid5@gmail.com>
2025-05-16 10:47:29 +0300

    Simplify Replit configuration for better compatibility

--- replit.nix
+++ replit.nix
@@ -1,17 +1,11 @@
 { pkgs }: {
     deps = [
-        pkgs.python310Full
-        pkgs.python310Packages.pip
-        pkgs.python310Packages.flask
-        pkgs.python310Packages.numpy
-        pkgs.python310Packages.pandas
-        pkgs.python310Packages.scikitlearn
-        pkgs.python310Packages.joblib
-        pkgs.python310Packages.requests
-        pkgs.python310Packages.transformers
+        pkgs.python3
+        pkgs.python3Packages.pip
+        pkgs.gcc
     ];
     env = {
-        PYTHONPATH = "${pkgs.python310Full}/bin/python3.10";
+        PYTHONPATH = "${pkgs.python3}/bin/python3";
         LANG = "en_US.UTF-8";
         PIP_ROOT_USER_ACTION = "ignore";
     };
