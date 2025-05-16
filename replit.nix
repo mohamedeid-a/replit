5605093dcf4d64bd456ec89a5f895fe41df683f4
mohamedeid-a <mohaamed.eid5@gmail.com>
2025-05-16 11:13:16 +0300

    تبسيط التكوين وإضافة تثبيت المكتبات

--- replit.nix
+++ replit.nix
@@ -1,8 +1,6 @@
 { pkgs }: {
     deps = [
-        pkgs.python38
-        pkgs.python38Packages.pip
-        pkgs.python38Packages.setuptools
-        pkgs.python38Packages.wheel
+        pkgs.python3
+        pkgs.pip
     ];
 }
