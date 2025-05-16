83074f3c65c034067e01a32529b1266d0add390c
mohamedeid-a <mohaamed.eid5@gmail.com>
2025-05-16 10:39:10 +0300

    Fix Replit configuration

--- replit.nix
+++ replit.nix
@@ -1,13 +1,11 @@
 { pkgs }: {
     deps = [
-        pkgs.python39
-        pkgs.python39Packages.pip
-        pkgs.python39Packages.flask
-        pkgs.python39Packages.numpy
-        pkgs.python39Packages.pandas
-        pkgs.python39Packages.scikitlearn
-        pkgs.python39Packages.requests
-        pkgs.python39Packages.transformers
-        pkgs.python39Packages.torch
+        pkgs.python3Full
+        pkgs.poetry
+        pkgs.gcc
     ];
+    env = {
+        PYTHONBIN = "${pkgs.python3Full}/bin/python3";
+        LANG = "en_US.UTF-8";
+    };
 }
