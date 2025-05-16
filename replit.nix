6ea7ea65c7d040cd1f8988bd39b7079bdcc52839
mohamedeid-a <mohaamed.eid5@gmail.com>
2025-05-16 11:18:46 +0300

    إعادة تكوين كاملة للمشروع

--- replit.nix
+++ replit.nix
@@ -1,6 +1,12 @@
 { pkgs }: {
-    deps = [
-        pkgs.python3
-        pkgs.pip
+    deps = with pkgs; [
+        python310
+        python310Packages.flask
+        python310Packages.numpy
+        python310Packages.pandas
+        python310Packages.scikit-learn
+        python310Packages.requests
+        python310Packages.pip
+        gcc
     ];
 }
