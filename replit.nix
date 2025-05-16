8176408cc0789ae0933bfb19e91fbd897a38f12a
mohamedeid-a <mohaamed.eid5@gmail.com>
2025-05-16 11:08:57 +0300

    Simplify configuration to minimum

--- replit.nix
+++ replit.nix
@@ -1,12 +1,8 @@
 { pkgs }: {
     deps = [
-        pkgs.python38Full
+        pkgs.python38
+        pkgs.python38Packages.pip
+        pkgs.python38Packages.setuptools
+        pkgs.python38Packages.wheel
     ];
-    env = {
-        PYTHONHOME = "${pkgs.python38Full}";
-        PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
-        LANG = "en_US.UTF-8";
-        STDERRED_PATH = "${pkgs.stderred}/lib/libstderred.so";
-        PYTHONPATH = ".";
-    };
 }
