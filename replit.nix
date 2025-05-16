e86ddb578229ec5ab86e5828d084872a7ffe57a7
mohamedeid-a <mohaamed.eid5@gmail.com>
2025-05-16 10:42:45 +0300

    Update Replit configuration with improved dependencies

--- replit.nix
+++ replit.nix
@@ -1,11 +1,18 @@
 { pkgs }: {
     deps = [
-        pkgs.python3Full
-        pkgs.poetry
-        pkgs.gcc
+        pkgs.python310Full
+        pkgs.python310Packages.pip
+        pkgs.python310Packages.flask
+        pkgs.python310Packages.numpy
+        pkgs.python310Packages.pandas
+        pkgs.python310Packages.scikitlearn
+        pkgs.python310Packages.joblib
+        pkgs.python310Packages.requests
+        pkgs.python310Packages.transformers
     ];
     env = {
-        PYTHONBIN = "${pkgs.python3Full}/bin/python3";
+        PYTHONPATH = "${pkgs.python310Full}/bin/python3.10";
         LANG = "en_US.UTF-8";
+        PIP_ROOT_USER_ACTION = "ignore";
     };
 }
